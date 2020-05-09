70fd0ff6d489176db5cd67864e0c06a703d2c580
Simon Boak <simon.boak@gmail.com>
2020-05-09 17:34:30 +0100

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # ebd
-ebd: Eight Bit Dump - A hexdump utility for 8 bit software development
+**ebd:** Eight Bit Dump - A hexdump utility for 8 bit software development
+
+A more useful alternative to modern hexdump utilities, *ebd* is designed for anyone using modern systems to develop software for older 8 bit processors.
+
+Its style is inspired by the Apple II system monitor program and outputs 8 bit bytes with the option to show ASCII characters too. It is fixed to 8 bytes per row, with a 16 bit address, which rolls over after each 64k. If you need more address space this isn't the tool for you.
+
+I basically failed in using format strings in *hexdump* which comes with macOS and decided it would be faster to write this little utility.
+
+Written in Python 2.7 because that was just easier for now.
+
+## Usage
+Just pass a file to *ebd* and you'll get a hexdump of it's contents:
+
+<img width="682" alt="example-basic" src="https://user-images.githubusercontent.com/1146324/81479384-c5967b80-921a-11ea-9a31-c69430bee5fd.png">
+
+### Custom starting address
+Use the `-a` parameter to pass a different starting address for the output (in decimal):
+
+<img width="682" alt="example-address" src="https://user-images.githubusercontent.com/1146324/81479389-cb8c5c80-921a-11ea-8139-d29368961658.png">
+
+### Display ASCII text
+Add the `-d` to display the ASCII text column in the output:
+
+<img width="682" alt="example-ascii" src="https://user-images.githubusercontent.com/1146324/81479393-cdeeb680-921a-11ea-9be5-77a2ce433550.png">
+
+
+## Installation
+
+You can either just call the Python script directly, or you could follow something like this for installing on *nix systems:
+
+`chmod a+x ebd.py`
+`sudo cp ebd.py /usr/local/bin/ebd`
+
+Now you can call `ebd [options]` from any directory on your system.
+
+## C Version
+Long term, I'd like to write a version of *ebd* in C, along with a few extra features at the same time. Maybe one day I'll get around to that.
---
+++ ebd.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+"""ebd: Eight Bit Dump - A hexdump utility for 8 bit software development
+
+http://github.com/simonboak/ebd
+
+Copyright (c) 2020 Simon Boak
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+"""
+
+from __future__ import print_function
+import argparse
+import binascii
+
+
+parser = argparse.ArgumentParser( description='ebd: Eight Bit Dump - A hexdump utility for 8 bit software development - http://github.com/simonboak/ebd - Version 1.0.0' )
+parser.add_argument( '-f', '--file', help='Input file to dump', required=True )
+parser.add_argument( '-a', '--address', type=int, help='Decimal starting address of output', required=False )
+parser.add_argument( '-d', '--displayascii', action='store_true', help='Display ASCII characters and show to the right of output', required=False )
+args = vars( parser.parse_args() )
+
+
+file = open( args['file'], 'r' )
+
+fileOpen = 1
+rowCount = 0
+
+# Get the starting byte address from arguments, otherwise 0
+if args['address'] == None:
+    byteCount = 0
+else:
+    byteCount = args['address']
+
+while fileOpen:
+    byteCountHex = hex( byteCount )
+    byteCountHexList = byteCountHex.split( 'x' ) # only want the digits after x from the 0x42 formatting style
+    print( byteCountHexList[1].zfill(4).upper(), end=': ' )
+    
+    asciiColumn = ''
+    
+    while rowCount < 8: # 8 bytes to a line
+        char = file.read( 1 )
+        if not char:
+            fileOpen = 0 # end of file reached
+            break
+        
+        print( binascii.b2a_hex( char ).upper(), end=' ' ) # convert byte to hex and in uppercase
+        
+        if args['displayascii'] == True and len( char ) > 0:
+            # If in printable ASCII range, then print the character, otherwise just a period
+            if ord( char ) > 31 and ord( char ) < 127:
+                asciiColumn = asciiColumn + char
+            else:
+                asciiColumn = asciiColumn + '.'
+        
+        byteCount += 1
+        rowCount += 1
+    
+    
+    
+    
+    # parse the bytes and display ascii
+    if args['displayascii'] == True:
+        # If the last line is less than 8 bytes long, add space to line up this column
+        if rowCount < 8:
+            spaces = 8 - rowCount
+            while spaces > 0:
+                print( '   ', end='' ) # print 3 empty spaces for each empty byte column
+                spaces = spaces - 1
+            
+        print( asciiColumn, end='' )
+    
+    
+    rowCount = 0
+    print() # New line
+    
+file.close()
